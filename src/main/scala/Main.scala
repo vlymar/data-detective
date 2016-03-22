c2074fdc31399bb28579008590914adfd4067e8f
vlymar <victorl@berkeley.edu>
2016-03-21 21:23:01 -0700

    Aggregate parsing is wrapped in Either. basic pretty printing is implemented.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,43 +1,58 @@
-import scala.collection.immutable.HashMap
-
 object Main {
   import scala.collection.Map
   import java.io.File
 
-
-  case class JsonObjectAggregate(attributes: Map[String, ValueAggregate], count: Int = 1) extends ValueAggregate {
-    def mkString: String = this.attributes.mkString("\n")
-  }
-
-  object JsonObjectAggregate {
-    def fromMap(jsonMap: Map[String, Any]): JsonObjectAggregate = {
-      val m = jsonMap.map {
-        case (k, v) => k -> ValueAggregate.makeAggregate(v)
-      }
-      JsonObjectAggregate(m)
-    }
-
-    def mergeAggregates(agg1: JsonObjectAggregate, agg2: JsonObjectAggregate): JsonObjectAggregate = {
-      // TODO: implement for real
-      agg1
+  def getListOfFiles(dir: String): List[File] = {
+    (new File(dir)) match {
+      case f if f.exists && f.isDirectory => f.listFiles.toList.map(_.toString).flatMap(getListOfFiles(_))
+      case f if f.exists && f.isFile => List(f)
+      case _ => List[File]()
     }
   }
 
   sealed trait ValueAggregate {
     val count: Int
+
+    def formatPretty(offset: Int = 0) = this.toString
   }
 
   object ValueAggregate {
-    def makeAggregate[A](value: A): ValueAggregate = {
+    def makeAggregate[A](value: A): Either[String, ValueAggregate] = {
       value match {
-        case null => NullAggregate(1)
-        case s: String => StringAggregate(s.length, s.length, s.length, 1)
-        case b: Boolean => BooleanAggregate.fromBoolean(b)
-        case l: List[Any] => ArrayAggregate(l.length, l.length, l.length, 1)
+        case null         => Right(NullAggregate(1))
+        case s: String    => Right(StringAggregate(s.length, s.length, s.length, 1))
+        case b: Boolean   => Right(BooleanAggregate.fromBoolean(b))
+        case l: List[Any] => Right(ArrayAggregate(l.length, l.length, l.length, 1))
         case m: Map[String, Any] => JsonObjectAggregate.fromMap(m)
 
-        case x => {println(s"${x.getClass.toString}"); NullAggregate(1)}
+        case x => Left(x.getClass.toString) // TODO: these arent getting printed with error prefixes (but are being pretty printed)
       }
+    }
+  }
+
+  case class JsonObjectAggregate(attributes: Map[String, Either[String, ValueAggregate]], count: Int = 1) extends ValueAggregate {
+    override def formatPretty(offset: Int = 0): String =
+      this.attributes.mapValues({
+        case Right(v) =>  v match {
+          case jOA: JsonObjectAggregate => jOA.formatPretty(offset + 4)
+          case x => x.formatPretty()
+        }
+        case Left(err) => err
+      }).mkString("\n" + (" " * offset))
+  }
+
+  object JsonObjectAggregate {
+    def fromMap(jsonMap: Map[String, Any]): Either[String, JsonObjectAggregate] = {
+      val m = jsonMap.map {
+        case (k, v) => k -> ValueAggregate.makeAggregate(v)
+      }
+      Right(JsonObjectAggregate(m))
+    }
+
+    def mergeAggregates(agg1: Either[String, JsonObjectAggregate],
+                        agg2: Either[String, JsonObjectAggregate]): Either[String, JsonObjectAggregate] = {
+      // TODO: implement for real
+      Left("not implemented yet")
     }
   }
 
@@ -54,14 +69,6 @@ object Main {
     }
   }
 
-  def getListOfFiles(dir: String): List[File] = {
-    (new File(dir)) match {
-      case f if f.exists && f.isDirectory => f.listFiles.toList.map(_.toString).flatMap(getListOfFiles(_))
-      case f if f.exists && f.isFile => List(f)
-      case _ => List[File]()
-    }
-  }
-
   def parseJson(body: String): Option[Any] = {
     import scala.util.parsing.json._
 
@@ -71,25 +78,29 @@ object Main {
   def analyzeJsonArray(arr: List[Any]): String =
     "array parsing not yet implemented"
 
-  def analyzeJsonObjects(objects: List[Map[String, Any]]): JsonObjectAggregate = {
-    objects.map(JsonObjectAggregate.fromMap(_)).reduce(JsonObjectAggregate.mergeAggregates)
+  def analyzeJsonObjects(objects: List[Map[String, Any]]): Either[String, JsonObjectAggregate] = {
+    objects
+      .map(JsonObjectAggregate.fromMap(_))
+      .reduce(JsonObjectAggregate.mergeAggregates)
   }
 
-  def analyzeJson(json: Option[Any]): Either[String, String] =
+  def analyzeJson(json: Option[Any]): Either[String, List[JsonObjectAggregate]] =
     json match {
       case Some(x) => x match {
-        case l: List[Any] => Right(analyzeJsonArray(l))
-        case m: Map[String, Any] => Right(JsonObjectAggregate.fromMap(m).mkString)
+        case l: List[Any] => Left(analyzeJsonArray(l))
+        case m: Map[String, Any] => JsonObjectAggregate.fromMap(m) match {
+          case Right(x) => Right(List(x))
+          case Left(y) => Left(y)
+        }
       }
       case None => Left("parser returned none")
-  }
+    }
 
-  def printAnalysis(analysis: Either[String, String]) = {
-    val out = analysis match {
-      case Right(info) => info
+  def formatAnalysis(analysis: Either[String, List[JsonObjectAggregate]]) = {
+    analysis match {
+      case Right(l) => l.map(_.formatPretty()).mkString("\n ---- \n")
       case Left(err) => "Error: " + err
     }
-    println(out)
   }
 
   def main(args: Array[String]) = {
@@ -112,7 +123,9 @@ object Main {
 
     // analyze parsed json
     val analysis = analyzeJson(parsed)
-    printAnalysis(analysis)
+    val prettyA = formatAnalysis(analysis)
+
+    println(prettyA)
   }
 }
 
